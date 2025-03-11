7047e0539b05e203deae7aba5fcaaa674bd1bd21
aristaanjani17 <iniiristaa@gmail.com>
2025-03-11 21:59:39 +0800

    Pertemuan02

---
+++ pertemuan02/src/pertemuan02/Pertemuan02.java
@@ -0,0 +1,24 @@
+package pertemuan02;
+
+/**
+ *
+ * @author aristaanjanii
+ * tgl:11 Maret 2025
+ */
+public class Pertemuan02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama = "arista anjani";
+       String nim = "2301010544";
+       
+       System.out.println("Pertemuan 2 Java");
+       System.out.println("~~~~~~~~~~~~~~~~");
+       System.out.println("Nama     : "+nama);
+       System.out.println("NIM  : "+nim);
+       System.out.printf("Nama saya %S dengan NIM %S, saat ini sedang belajar pemrograman bahasa Java",nama,nim);
+    }
+    
+}
